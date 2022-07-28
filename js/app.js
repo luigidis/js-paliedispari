8cba34a08bcf127e1d5c8c33249b380d8de91f88
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-28 15:46:53 +0200

    refactoring file js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
-console.log('yo');
+// console.log('yo');
 // Chiedo la parola all'utente
 const parolaPalindroma = prompt('Digita una parola');
-// console.log(parolaPalindroma);
-// console.log(typeof parolaPalindroma)
 function isPalindroma(parola) {
     //Prendo ogni lettera singolarmente e le metto in un array
     const lettereParola = parola.split('');
-    console.log(lettereParola);
-    //Inverto l'array creato e lo metto in un'altro array invertito
-    // const lettereInvertite = lettereParola.reverse('');
-    // console.log(lettereInvertite);
-    // console.log(typeof lettereInvertite);
     // Confronto ogni lettera dell'array
-    
     for (let i = 0; i < lettereParola.length; i++) {
         console.log(lettereParola[i], lettereParola[lettereParola.length - 1])
         if (lettereParola[i] !== lettereParola[lettereParola.length - 1]) {
@@ -23,6 +15,9 @@ function isPalindroma(parola) {
     }
 }
 console.log(isPalindroma(parolaPalindroma))
+    
+   
+    
     
     
 
