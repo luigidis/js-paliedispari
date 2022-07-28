55a58bc42de8207935ebe1b23e9c6bf0a66ea96c
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-28 15:45:20 +0200

    Fixed

--- js/app.js
+++ js/app.js
@@ -8,24 +8,24 @@ function isPalindroma(parola) {
     const lettereParola = parola.split('');
     console.log(lettereParola);
     //Inverto l'array creato e lo metto in un'altro array invertito
-    const lettereInvertite = lettereParola.reverse('');
-    console.log(lettereInvertite);
+    // const lettereInvertite = lettereParola.reverse('');
+    // console.log(lettereInvertite);
     // console.log(typeof lettereInvertite);
     // Confronto ogni lettera dell'array
     
     for (let i = 0; i < lettereParola.length; i++) {
-        console.log(lettereParola[i], lettereInvertite[i])
-        if (lettereParola[i] !== lettereInvertite[i]) {
+        console.log(lettereParola[i], lettereParola[lettereParola.length - 1])
+        if (lettereParola[i] !== lettereParola[lettereParola.length - 1]) {
             return false
         } else {
             return true
         }
     }
-    
 }
+console.log(isPalindroma(parolaPalindroma))
+    
     
 
-console.log(isPalindroma(parolaPalindroma))
 
 
 
