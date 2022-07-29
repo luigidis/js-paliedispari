eb82408d2619cb6bb4716ca97b52ccf654ee88f3
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-29 09:55:19 +0200

    Palindroma fixed

--- js/app.js
+++ js/app.js
@@ -2,19 +2,20 @@
 // Chiedo la parola all'utente
 const parolaPalindroma = prompt('Digita una parola');
 function isPalindroma(parola) {
-    //Prendo ogni lettera singolarmente e le metto in un array
-    const lettereParola = parola.split('');
-    // Confronto ogni lettera dell'array
-    for (let i = 0; i < lettereParola.length; i++) {
-        console.log(lettereParola[i], lettereParola[lettereParola.length - 1])
-        if (lettereParola[i] !== lettereParola[lettereParola.length - 1]) {
-            return false;
-        } else {
-            return true;
-        }
+//Prendo ogni lettera singolarmente e le metto in un array
+const lettereParola = parola.split('');
+console.log(lettereParola);
+// Confronto ogni lettera dell'array
+for (let i = 0; i < lettereParola.length; i++) {
+    console.log(lettereParola[i], lettereParola[(lettereParola.length - 1) - i])
+    if (lettereParola[i] !== lettereParola[(lettereParola.length - 1) - i]) {
+        return false;
+    } else {
+        return true;
     }
-}
+}}
 console.log(isPalindroma(parolaPalindroma));
+
 
 
     
--- js/pari_dispari.js
+++ js/pari_dispari.js
@@ -3,6 +3,19 @@
 const pariDispari = prompt('Pari o Dispari')
 const numeroDigitato = parseInt(prompt('Digita un numero compreso tra 1 e 5'));
 console.log(numeroDigitato, pariDispari)
+
+// Constrollo per vedere se il mio utente ha digitato un numero compreso tra 1 e 5
+// Creo un array che ha
+ const arraycheck = [1, 2, 3, 4, 5]
+ console.log(arraycheck)
+ for ( let i = 0; i < arraycheck.length; i++) {
+     if (numeroDigitato !== arraycheck[i]) {
+        alert ('Digita un numero Valido')
+        break
+        } 
+}
+
+
 // Costanti che ho stabilito precedentemente per il mio minimo e il mio massimo
 const MIN = 1
 const MAX = 5
@@ -25,8 +38,8 @@ function isEven(num) {
         return true
     } 
     return false
-
 }
+
 console.log(isEven(num))
 // metto il risultato true o false dentro una funzione
 const risultato = isEven(num);
@@ -42,10 +55,3 @@ if (((risultato === true) && (pariDispari === 'Pari')) || ((risultato === false)
 
 
 
-//Constrollo per vedere se il mio utente ha digitato un numero compreso tra 1 e 5
-// Creo un array che ha
-// const arraycheck = [1, 2, 3, 4, 5]
-// console.log(arraycheck)
-// for ( let i = 0; i < arrayckeck.length; i++) {
-//     if numeroDigitato === arraycheck[i] 
-// }
